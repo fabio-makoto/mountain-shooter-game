8ac72049275070e68c19ee4bec4f0eb123e4212d
fabio-makoto <nakasimafabio@gmail.com>
2026-02-24 23:31:48 -0300

    adding classes created for starting game

---
+++ code/game.py
@@ -0,0 +1,19 @@
+import pygame
+from menu import Menu
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        window = pygame.display.set_mode(size=(600, 480))
+    
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+
+            # for event in pygame.event.get():
+            #     if event.type == pygame.QUIT:
+            #         pygame.quit()  # close window
+            #         quit()  #  end pygame
