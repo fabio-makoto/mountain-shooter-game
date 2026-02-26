20172e2d25aef765883c9a77b40d737cfd60860e
fabio-makoto <nakasimafabio@gmail.com>
2026-02-26 20:43:42 -0300

    added a game instace to run the game

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import pygame
+from code.game import Game
 
+game = Game()
 
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()  # close window
-            quit()  #  end pygame
+game.run()
